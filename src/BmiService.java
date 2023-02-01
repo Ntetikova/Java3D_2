241bee8bae0f452f618c9c28c0144b2d34274ddc
Тетикова Наталья <ntetikva@gmail.com>
2023-02-01 15:08:32 +0300

    Changed code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,25 +1,7 @@
-/*public class BmiService {
-    public int calculate(int I) {
-        double m = 170;
-        double h = 65;
-        double imt = m / h * h;
-        return imt;
-    }
 
-}
-*/
 public class BmiService {
     public double calculate(double weight, double height) {
         double index = weight / (height * height);
-        if (index < 18.5) {
-            System.out.println("Недовес: меньше чем 18.5");
-        } else if (index < 25) {
-            System.out.println("Нормальный: между 18.5 и 25");
-        } else if (index < 30) {
-            System.out.println("Избыточный вес: между 25 и 30");
-        } else {
-            System.out.println("Ожирение: 30 или больше");
-        }
         return index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,24 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 65; // вес
-        double height = 1.7; // рост
-        double index = service.calculate(weight, height);
-        System.out.println(index);
+        /*
+        weight = 65; // вес
+        height = 1.7; // рост
+        */
+        System.out.println();
+        double imt = service.calculate(65, 1.7);
+        if (imt < 18.5) {
+            System.out.println("Ваш индекс составляет: " + imt);
+            System.out.println("Недовес: меньше чем 18.5");
+        } else if (imt < 25) {
+            System.out.println("Ваш индекс составляет: " + imt);
+            System.out.println("Норма: между 18.5 и 25");
+        } else if (imt < 30) {
+            System.out.println("Ваш индекс составляет: " + imt);
+            System.out.println("Избыточный вес: между 25 и 30");
+        } else {
+            System.out.println("Ваш индекс составляет: " + imt);
+            System.out.println("Ожирение: 30 или больше");
+        }
     }
 }
